b30478ead769eb0efbd1126e4618c08c2815fd93
andrew.napier <andrew.napier@runbox.com>
2022-02-12 22:11:34 +1000

    Initial commit

---
+++ echo/Hello.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Sample
+{
+	public class HelloCommand : IRequest<string>
+	{
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class HelloHandler : IRequestHandler<HelloCommand, string>
+    {
+        public Task<string> Handle(HelloCommand request, CancellationToken cancellationToken)
+        {
+            string responseMessage =
+                $"Hello {request.Name}. I find it hard to believe you are {request.Age} years old!";
+            return Task.FromResult(responseMessage);
+        }
+    }
+
+
+}
+
---
+++ echo/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MediatR;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+[assembly: FunctionsStartup(typeof(Sample.Startup))]
+namespace Sample
+{
+
+	public class Startup : FunctionsStartup
+	{
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services
+                .AddMvcCore();
+
+            builder.Services
+                .AddMediatR(typeof(Startup))
+                .AddLogging(loggingBuilder =>
+                {
+                    loggingBuilder.SetMinimumLevel(LogLevel.Debug);
+                });
+        }
+    }
+}
+
